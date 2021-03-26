edb1762cea77376dcee8326be5b7c70fcb5b232e
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-03-26 21:24:25 +0900

    🐛 form validation

--- src/section/Contact/index.js
+++ src/section/Contact/index.js
@@ -7,53 +7,81 @@ export default function Contact() {
   const onSubmit = data => console.log(data);
   console.log(errors);
 
-    return (
-      <section className='section-book' id='book-now'>
-        <div className='row'>
-          <div className='book'>
-            <div className='book__form'>
-              <div className='u-margin-bottom-medium'>
-                <h2 className='heading-secondary' style={{ fontSize: '4rem' }}>
-                  お問い合わせ
-                </h2>
-              </div>
-              <form onSubmit={handleSubmit(onSubmit)} data-netlify='true'>
-                <p className='form__group'>
-                  <input
-                    className='form__input'
-                    type='text'
-                    name='name'
-                    placeholder={'お名前'}
-                    ref={register({ required: true })}
-                  />
-                </p>
-                <p className='form__group'>
-                  <input
-                    className='form__input'
-                    type='email'
-                    name='email'
-                    placeholder={'メールアドレス'}
-                    ref={register({ required: true })}
-                  />
-                </p>
-                <p className='form__group'>
-                  <textarea
-                    className='form__input'
-                    rows='7'
-                    name='message'
-                    placeholder={'メッセージ'}
-                    ref={register({ required: true })}
-                  />
-                </p>
-                <p className='form__group form-submit'>
-                  <div className='form__group'>
-                    <input type='submit' className='btn btn--green' />
-                  </div>
-                </p>
-              </form>
+  return (
+    <section className='section-book' id='book-now'>
+      <div className='row'>
+        <div className='book'>
+          <div className='book__form'>
+            <div className='u-margin-bottom-medium'>
+              <h2 className='heading-secondary' style={{ fontSize: '4rem' }}>
+                お問い合わせ
+              </h2>
             </div>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              name='Contact'
+              method='POST'
+              netlify-honeypot='bot-field'
+              data-netlify='true'
+              action='/'
+            >
+              <p className='form__group'>
+                <input
+                  className='form__input'
+                  type='text'
+                  name='name'
+                  placeholder={'お名前'}
+                  ref={register({
+                    required: true,
+                    max: 255,
+                    min: 3,
+                    maxLength: 80,
+                  })}
+                />
+                {errors.name && '名前を入力してください'}
+              </p>
+              <p className='form__group'>
+                <input
+                  className='form__input'
+                  type='email'
+                  name='email'
+                  placeholder={'メールアドレス'}
+                  ref={register({
+                    required: true,
+                    max: 41,
+                    min: 4,
+                    maxLength: 255,
+                    pattern: /^\S+@\S+$/i,
+                  })}
+                />
+                {errors.email && 'メールアドレスを入力してください'}
+              </p>
+              <p className='form__group'>
+                <textarea
+                  className='form__input'
+                  rows='7'
+                  name='message'
+                  placeholder={'メッセージ'}
+                  ref={register({
+                    required: true,
+                    max: 1000,
+                    min: 10,
+                    maxLength: 7,
+                  })}
+                />
+                {errors.message && 'メールアドレスを入力してください'}
+              </p>
+              <p className='form__group form-submit'>
+                <div className='form__group'>
+                  <button type='submit' className='btn btn--green'>
+                    送信
+                  </button>
+                </div>
+              </p>
+            </form>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
+}
