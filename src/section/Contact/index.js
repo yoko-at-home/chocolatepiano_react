3f27b6b1982ccec8eb5cc7612eee19331641ce20
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-03-29 17:16:34 +0900

    🐛

--- src/section/Contact/index.js
+++ src/section/Contact/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { NetlifyForm, Honeypot } from 'react-netlify-forms';
+import {
+  useNetlifyForm,
+  NetlifyFormProvider,
+  NetlifyFormComponent,
+  Honeypot,
+} from 'react-netlify-forms';
 import { useForm } from 'react-hook-form';
 import './styles.css';
 
 function Contact() {
-  const { errors, register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset, errors } = useForm({ mode: 'onBlur' });
+  const netlify = useNetlifyForm({
+    name: 'react-hook-form',
+    action: '/thanks',
+    honeypotName: 'bot-field',
+    onSuccess: (response, context) => {
+      console.log('Successfully sent form data to Netlify Server');
+    },
+  });
+  const onSubmit = (data) => netlify.handleSubmit(null, data);
+
+  const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i;
 
   return (
     <section className='section-book' id='book-now'>
@@ -16,95 +32,85 @@ function Contact() {
                 お問い合わせ
               </h2>
             </div>
-            <NetlifyForm
-              name='Contact'
-              action='/thanks'
-              honeypotName='bot-field'
-              onSuccess={(response, context) => {
-                console.log('Successfully sent form data to Netlify Server');
-                context.formRef.current.reset();
-              }}
-            >
-              {({ handleChange, success, error }) => (
-                <>
-                  <Honeypot />
-                  {success && (
-                    <p sx={{ variant: 'alerts.success', p: 3 }}>
-                      Thanks for contacting us!
-                    </p>
-                  )}
-                  {error && (
-                    <p sx={{ variant: 'alerts.muted', p: 3 }}>
-                      Sorry, we could not reach servers. Because it only works
-                      on Netlify, our GitHub demo does not provide a response.
-                    </p>
-                  )}
-                  <p className='form__group'>
-                    <input
-                      className='form__input'
-                      type='text'
-                      name='name'
-                      placeholder={'お名前'}
-                      onChange={handleChange}
-                      ref={register({
-                        required: true,
-                        max: 255,
-                        min: 3,
-                        maxLength: 80,
-                      })}
-                    />
-                    {errors.name && '名前を入力してください'}
+            <NetlifyFormProvider {...netlify}>
+              <NetlifyFormComponent onSubmit={handleSubmit(onSubmit)}>
+                <Honeypot />
+                {netlify.success && <p>お問い合わせありがとうございます。</p>}
+                {netlify.error && (
+                  <p>
+                    サーバーに接続できません。時間をおいて再度お試しください。
                   </p>
-                  <p className='form__group'>
-                    <input
-                      className='form__input'
-                      type='email'
-                      name='email'
-                      placeholder={'メールアドレス'}
-                      onChange={handleChange}
-                      ref={register({
-                        required: true,
-                        max: 41,
-                        min: 4,
-                        maxLength: 255,
-                        pattern: /^\S+@\S+$/i,
-                      })}
-                    />
+                )}
+                <p className='form__group'>
+                  <div className='form__input'>
+                    {errors.name && '入力してください'}
+                  </div>
+                  <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    ref={register({
+                      required: '入力してください',
+                      max: 255,
+                      min: 5,
+                      maxLength: 80,
+                    })}
+                    placeholder={'お名前'}
+                    className='form__input'
+                  />
+                </p>
+                <p className='form__group'>
+                  <div className='form__input'>
                     {errors.email && '入力してください'}
-                  </p>
-                  <p className='form__group'>
-                    <textarea
-                      className='form__input'
-                      rows='7'
-                      name='message'
-                      placeholder={'メッセージ'}
-                      onChange={handleChange}
-                      ref={register({
-                        required: true,
-                        max: 1000,
-                        min: 10,
-                        maxLength: 7,
-                      })}
-                    />
+                  </div>
+                  <input
+                    placeholder='メールアドレス'
+                    type='email'
+                    name='email'
+                    id='email'
+                    ref={register({
+                      required: 'Email is required',
+                      pattern: {
+                        value: EMAIL_REGEX,
+                        message: 'Invalid email address',
+                      },
+                    })}
+                    className='form__input'
+                  />
+                </p>
+                <p className='form__group'>
+                  <div className='form__input'>
                     {errors.message && '入力してください'}
-                  </p>
-                  <p className='form__group form-submit'>
-                    <div className='form__group'>
-                      <button
-                        type='submit'
-                        className='btn btn--green'
-                        sx={{ variant: 'buttons.success' }}
-                      >
-                        送信
-                      </button>
-                      {/* <button type="reset" sx={{ variant: "buttons.danger" }}>
-                  Reset
-                </button> */}
-                    </div>
-                  </p>
-                </>
-              )}
-            </NetlifyForm>
+                  </div>
+                  <textarea
+                    className='form__input'
+                    rows='7'
+                    id='message'
+                    name='message'
+                    placeholder={'メッセージ'}
+                    ref={register({ required: true, maxLength: 1000 })}
+                  />
+                </p>
+                <p className='form__group form-submit'>
+                  <div className='form__group'>
+                    <button
+                      type='submit'
+                      className='btn btn--green'
+                      ref={register}
+                    >
+                      送 信
+                    </button>
+                    <button
+                      type='reset'
+                      onClick={() => reset()}
+                      className='btn btn--green'
+                    >
+                      リセット
+                    </button>
+                  </div>
+                </p>
+              </NetlifyFormComponent>
+            </NetlifyFormProvider>
           </div>
         </div>
       </div>
